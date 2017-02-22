1ce46c488fa3b512920f9f04095bc04cc3d6c1d8
mainyanim <alicejarmusch@gmail.com>
2017-02-22 22:20:44 +0200

    changes in views

--- django-project/billboard/urls.py
+++ django-project/billboard/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name="signup"),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^signup/success/$', views.success, name = "success"),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    # url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^login/$',views.index, name='index'),
     url(r'^logout/$', authviews.logout, {'next_page' : '/billboard'}, name = 'logout')
 ]
--- django-project/billboard/views.py
+++ django-project/billboard/views.py
@@ -22,23 +22,23 @@ def index(request):
             post.published_date = timezone.now()
             post.save()
     else:
-        form = PostForm()   
+        form = PostForm()
     return render(request, 'billboard/index.html', {'posts': posts, 'form':form})
 
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
-            # return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'billboard/post_edit.html', {'form': form})
+# def post_edit(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     if request.method == "POST":
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             # return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm(instance=post)
+#     return render(request, 'billboard/post_edit.html', {'form': form})
 
 #there should be defined something like post_detail function - it causes an error but it wasn't described in tutorial
 def check_user(request):
